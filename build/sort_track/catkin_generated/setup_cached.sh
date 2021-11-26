b55320475481a91207efb0afddfde17a0414d002
s3rgiux <mechatronomus@hotmail.com>
2021-11-26 17:03:43 +0900

    added first try lane detection

--- build/sort_track/catkin_generated/setup_cached.sh
+++ build/sort_track/catkin_generated/setup_cached.sh
@@ -8,6 +8,9 @@
 
 # modified environment variables
 export CMAKE_PREFIX_PATH="/home/xavier/catkin_ws/devel/.private/sort_track:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/xavier/catkin_ws/devel/.private/sort_track/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/xavier/catkin_ws/devel/.private/sort_track/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD='/home/xavier/catkin_ws/build/sort_track'
+export PYTHONPATH="/home/xavier/catkin_ws/devel/.private/sort_track/lib/python2.7/dist-packages:$PYTHONPATH"
 export ROSLISP_PACKAGE_DIRECTORIES="/home/xavier/catkin_ws/devel/.private/sort_track/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
 export ROS_PACKAGE_PATH="/home/xavier/catkin_ws/src/sort-deepsort-yolov3-ROSS/sort_track:$ROS_PACKAGE_PATH"
